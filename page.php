70cadc8e6fa9ba4ace6d69fce2241f9b8217dad2
David Kinloch <dkinloch@gmail.com>
2019-05-09 19:47:04 +0200

    generic page styling

--- page.php
+++ page.php
@@ -5,12 +5,14 @@
   ?>
      <div class="section no-margin banner banner-text" id="partnername-banner" >
         <div class="container">
-          <div class="row">
-            <div class="col s12">
-              <h1><?php the_title(); ?></h1>
-              <div class="text-column">
-                <h2><?php echo $leading_paragraph ?></h2>
-              </div>
+          <div class="row no-margin">
+            <div class="col s12 m8 offset-m2">
+              <?php if ($leading_paragraph): ?>
+                <h1><?php the_title(); ?></h1>
+                <h2><?php echo $leading_paragraph ;?></h2>
+              <?php else : ?>
+                <h1 class="no-margin"><?php the_title(); ?></h1>
+              <?php endif; ?>
             </div>
           </div>
       </div>
@@ -23,7 +25,7 @@
         <div class="row">
           <div class="col s12">
             <div class="row"> 
-              <div class="col s12 m8 offset-m2">
+              <div class="col s12 m8 offset-m2 cms-text">
                 <?php the_content(); ?>
               </div>
             </div>
