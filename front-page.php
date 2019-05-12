25c47529daac068bc14021fa39c12a00a5d90fb3
David Kinloch <dkinloch@gmail.com>
2019-05-12 19:13:15 +0200

    Added featured image to homepage

--- front-page.php
+++ front-page.php
@@ -1,14 +1,15 @@
 <?php include 'header.php'; ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); 
   $leading_paragraph = get_field('leading_paragraph');
+  $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); 
   
 ?>
-<div class="section no-pad-bot no-margin banner" id="index-banner" style="background-image: url(https://dummyimage.com/1440x400/ccc/333.jpg);">
+<div class="section no-pad-bot no-margin banner" id="index-banner" style="background-image: url(<?php echo $featured_img_url ?>);">
   <div class="container valign-wrapper">
     <div class="row center">
       <div class="col s12 m8 offset-m2 light-blue">
         <div class="banner-center"> 
-          <h1 class="white-text">PeaceNexus</h1>
+          <h1 class="white-text"><?php the_title();?></h1>
           <h2 class="white-text"><?php echo $leading_paragraph ;?></h2>
         </div>
       </div>
