bd940c30cb1accdbd95e48f2dbca79d6c7c97142
David Kinloch <dkinloch@gmail.com>
2019-06-03 22:58:49 +0200

    wider single page

--- page.php
+++ page.php
@@ -25,7 +25,7 @@
         <div class="row">
           <div class="col s12">
             <div class="row"> 
-              <div class="col s12 m8 offset-m2 cms-text">
+              <div class="col s12 m10 offset-m1 cms-text">
                 <?php the_content(); ?>
               </div>
             </div>
