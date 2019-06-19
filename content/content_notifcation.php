2dd62444a947663d163e31424bd56733aabf6275
David Kinloch <dkinloch@gmail.com>
2019-06-19 10:55:46 +0200

    notification bar on mobile

--- content/content_notifcation.php
+++ content/content_notifcation.php
@@ -1,5 +1,5 @@
 <?php if( get_field('notification_bar_enabled') ): ?>
-  <div class="notification row hide-on-med-and-down">
+  <div class="notification row">
     <span class="center col s12"><?php the_field('content'); ?></span>
   </div>
 <?php endif; ?>
