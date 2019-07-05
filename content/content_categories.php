70cb9688076a89048ae02e38d3e3bfa0a7241103
nlouw <nicola@squareblok.co.uk>
2019-07-05 11:45:34 +0200

    Services as tags
    Services changed from cats to tags

--- content/content_categories.php
+++ content/content_categories.php
@@ -1,5 +1,6 @@
 <?php
 $postCategories = get_the_category($currentPostId);
+$postTags = get_the_terms($currentPostId, 'post_tag');
 
 $categories = get_categories( array(
   'orderby' => 'name',
@@ -23,23 +24,22 @@ foreach( $categories as $category ) {
 	}
 } 
 
-$categories = get_categories( array(
-	'orderby' => 'name',
-	'parent' => 9
+$services = get_terms( 'post_tag', array(
+	'orderby' => 'name' 
 ) );
  
-foreach( $categories as $category ) {
-	foreach( $postCategories as $postCategory ) {
-		if ($postCategory->term_id == $category->term_id) {
-			$category_link = sprintf( 
+foreach( $services as $service ) {
+	foreach( $postTags as $postTag ) {
+		if ($postTag->term_id == $service->term_id) {
+			$service_link = sprintf( 
 				'<a class="btn waves-effect waves-light" href="%1$s" alt="%2$s">%3$s</a>',
 				//esc_url( get_category_link( $category->term_id ) ),
-				'/highlights?id=' . $category->slug,
-				esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
-				esc_html( $category->name )
+				'/highlights?id=' . $service->slug,
+				esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $service->name ) ),
+				esc_html( $service->name )
 			);
      
-	      	echo sprintf( esc_html__( '%s', 'textdomain' ), $category_link );
+	      	echo sprintf( esc_html__( '%s', 'textdomain' ), $service_link );
 			break;
 		}
 	}	
